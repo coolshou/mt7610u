626a7a988fd1af57a3ea0cfc10cb3020b119f5fa
coolshou <coolshou2060@gmail.com>
2017-02-13 13:47:36 +0800

    fix get VERSION rule

--- build-deb.sh
+++ build-deb.sh
@@ -2,7 +2,7 @@
 
 make clean
 #fakeroot debian/rules clean
-VERSION=`cat include/version.h | grep DRIVER_VERSION | cut -d" " -f3 | cut -d"\"" -f2`
+VERSION=`cat include/version.h | grep DRIVER_VERSION | cut -d" " -f2 | cut -d"\"" -f2`
 DRIVER_BUILD_VERSION=`cat os/linux/config.mk | grep 'STA_DRIVER_BUILD' | cut -d' ' -f3 |cut -d'"' -f2 | head -n1`
 sed -e "s|#VERSION#|${VERSION}|" \
 	-e "s|#DRIVER_BUILD_VERSION#|${DRIVER_BUILD_VERSION}|" \
